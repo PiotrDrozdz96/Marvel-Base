36dbf2f9706e401317d34bda7cefd1bfad271998
Piotr Drożdż <piotr.drozdz0@op.pl>
2018-02-13 22:53:45 +0100

    objective and working volumesMode

--- Scripts/generatorCtrl.js
+++ Scripts/generatorCtrl.js
@@ -2,8 +2,7 @@ angular
   .module('app')
   .controller('generatorCtrl', function($scope,Base,Dialog){
 
-    //get Data
-
+    //Data
     $scope.modes = {view: {id:"view", value:"Wyświetl"},
                     delete: {id:"delete", value:"Usuwanie"},
                     edit: {id:"edit", value: "Edycja /Dodawanie"},
@@ -11,13 +10,18 @@ angular
                     array: {id:"array", value: "Array"},
                     volumes: {id:"volumes", value: "Twórz Tomy"}
     }
-
-    $scope.setMode = function(mode){$scope.activeMode = mode}
+    $scope.activeMode = "view"
+    $scope.setMode = function(mode){
+      $scope.activeMode = mode
+      $scope.series.type = mode=="volumes" ? "tomy" : "zeszyty"
+      if($scope.volumes.id) $scope.volumes.pack()
+    }
 
     Base.get("Base/Comics/base.JSON").then(function(data){
       $scope.base = data;
     })
 
+    //Categories
     Base.get("Base/Comics/categories.JSON").then(function(categoriesData){
       $scope.waves = {
         data: categoriesData,
@@ -43,6 +47,7 @@ angular
       Base.get("Base/Comics/series.JSON").then(function(seriesData){
         $scope.series = {
           data: seriesData,
+          type: "zeszyty",
           selected: categoriesData[$scope.waves.selected].series[0].title,
           new: "",
           set: function(){this.selected = $scope.waves.data[$scope.waves.selected].series[0].title},
@@ -64,16 +69,17 @@ angular
           }
         }
       })
-      $scope.selectedType = "zeszyty"
     })
 
-    //element functions
+    //Element functions
+    let argsToId = (obj) => [obj.title,obj.volume,obj.number]
+
     $scope.element = {
       exist: (id) => $scope.base[id] ? true : false,
       changeCover: function (id){
         $scope.base[id].cover = prompt('Cover link',$scope.base[id].cover)
       },
-      selected: () => $scope.series.data[$scope.series.selected].zeszyty,
+      selected: () => $scope.series.data[$scope.series.selected][$scope.series.type],
       deleteRest: function(end){
         while (this.selected().length != end){
           delete $scope.base[this.selected().pop()]
@@ -84,49 +90,51 @@ angular
         this.selected().splice(index,1)
       },
       add: function(id,index){
-        $scope.base[id]=$scope.newElement
+        $scope.base[id]=$scope.newElement.get()
+        if($scope.series.type=="tomy") $scope.base[id].children=[]
         this.selected().splice(index,0,id)
-        setNewElement()
+        $scope.newElement.set()
+      },
+      changeId: function(id,index){
+        let newId = Base.createId(...argsToId($scope.base[id]))
+        if($scope.base[newId]){
+          alert("Istnieje taki element")
+          $scope.base[id] = Object.assign($scope.base[id], Base.separateId(id))
+        }
+        else{
+          $scope.base[id].id = newId
+          $scope.base[newId] = $scope.base[id]
+          delete $scope.base[id]
+          this.selected()[index] = newId
+        }
       }
     }
 
-    $scope.changeId = function(id,index){
-      let newId = Base.createId(...argsToId($scope.base[id]))
-      if($scope.base[newId]){
-        alert("Istnieje taki element")
-        $scope.base[id] = Object.assign($scope.base[id], Base.separateId(id))
-      }
-      else{
-        $scope.base[id].id = newId
-        $scope.base[newId] = $scope.base[id]
-        delete $scope.base[id]
-        $scope.element.selected()[index] = newId
-      }
+    //New element
+    $scope.newElement = {
+      get:function(){ return{title:this.title,volume:this.volume,number:this.number,id:this.id,
+            series:[$scope.series.selected],subTitle:this.subTitle,
+            publishedDate:this.publishedDate,cover:this.cover}},
+      set: function(){
+        this.title="title"; this.volume=""; this.number=""; this.id= "";
+        this.subTitle="subTitle", this.publishedDate="publishedDate"; this.cover=""
+      },
+      changeCover: function(){this.cover = prompt('Cover link',this.cover)}
     }
+    $scope.newElement.set()
 
-    let setNewElement = function(){$scope.newElement = {"title":"title","volume":"","number":"","id":"","series":[$scope.selectedSeries],"subTitle":"subTitle","publishedDate":"publishedDate","cover":""}}
-    let argsToId = (obj) => [obj.title,obj.volume,obj.number]
-
-
-    $scope.setMode("view")
-    $scope.unPackVolume = {id:"",index:0,wave:"",series:""}
-    setNewElement();
-
-    $scope.changeNewElementCover = function (){
-      $scope.newElement.cover = prompt('Cover link',$scope.newElement.cover)
-    }
-
+    //Dialogs
     $scope.dialog = Object.assign(Dialog,{
-      setNewElement: () => setNewElement(),
+      setNewElement: () => $scope.newElement.set(),
 
       tryAddElement: function(index){
-        let id = Base.createId(...argsToId($scope.newElement))
+        let id = Base.createId(...argsToId($scope.newElement.get()))
         if($scope.element.exist(id)){ $scope.id = id; Dialog.open('conflictElements',[index]) }
         else $scope.element.add(id,index)
       },
 
       replaceElement: function(index){
-        let newId = Base.createId(...argsToId($scope.newElement))
+        let newId = Base.createId(...argsToId($scope.newElement.get()))
         if($scope.id!=newId){
           if($scope.element.exist(newId)){
             $scope.id = newId; Dialog.open('conflictElements',[index])
@@ -148,41 +156,35 @@ angular
 
     //Generate volumes
 
-    $scope.unPack = function(id){
-      $scope.id = id
-      $scope.unPackVolume = {
-        "id": id,
-        index:$scope.base[id].children.length,
-        wave: $scope.waves.selected,
-        series: $scope.series.selected
+    $scope.volumes = {
+      id: "", index: 0, wave: "", series: "",
+      setIndex: function(index){this.index=index},
+      unPack: function(id){
+        $scope.id = this.id = id
+        this.index = $scope.base[id].children.length
+        this.wave = $scope.waves.selected
+        this.series =  $scope.series.selected
+      },
+      pack: function(){
+        $scope.base[$scope.id].series = $scope.base[$scope.id].children.reduce(
+          (total,current)=> total.findIndex((c)=>c==$scope.base[current].series[0])==-1 ?
+            total.concat([$scope.base[current].series[0]]) : total
+        ,[] )
+        $scope.id = this.id = ""
+        $scope.waves.selected = this.wave
+        $scope.series.selected = this.series
+      },
+      addChild: function(id){
+        if($scope.base[this.id].children.findIndex((c) => c==id)==-1){
+          $scope.base[this.id].children.splice(this.index,0,id)
+          this.index++
+        }
+        else alert("Element znajduje się już na liście")
+      },
+      removeChild: function(index){
+        $scope.base[this.id].children.splice(index,1)
+        if(this.index>index) this.index--
       }
     }
-
-    $scope.unPackIndex = function(index){
-      $scope.unPackVolume.index = index
-    }
-
-    $scope.pack = function(){
-      $scope.id = ""
-      $scope.unPackVolume.id = ""
-      $scope.waves.selected = $scope.unPackVolume.wave
-      $scope.series.selected = $scope.unPackVolume.series
-    }
-
-    $scope.removeChild = function(index){
-      $scope.base[$scope.unPackVolume.id].children.splice(index,1)
-      if($scope.unPackVolume.index>index) $scope.unPackVolume.index--
-    }
-
-    $scope.addChild = function(id){
-      if($scope.base[$scope.unPackVolume.id].children.findIndex((c) => c==id)==-1){
-        $scope.base[$scope.unPackVolume.id].children.splice($scope.unPackVolume.index,0,id)
-        $scope.unPackVolume.index++
-      }
-      else alert("Element znajduje się już na liście")
-    }
-
-    //Dodawanie nowych tomów
-    //Usuwanie Serii i Nurtów
 
   })
